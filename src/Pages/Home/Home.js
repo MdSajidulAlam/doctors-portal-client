38871ce7988a97f86cbb32452d0a8b57ee935bb2
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-14 02:52:34 +0600

    Exceptional care added in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import ExceptionalCare from './ExceptionalCare';
 import Info from './Info';
 import Services from './Services';
 
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <Info />
             <Services />
+            <ExceptionalCare />
         </div>
     );
 };
