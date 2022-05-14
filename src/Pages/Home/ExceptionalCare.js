ef2556cbddf7667157f8337f80a938264a20f6d4
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-14 19:06:32 +0600

    Responsive done

--- src/Pages/Home/ExceptionalCare.js
+++ src/Pages/Home/ExceptionalCare.js
@@ -6,8 +6,8 @@ const ExceptionalCare = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row">
-                <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" alt='' />
-                <div className='px-40'>
+                <img src={treatment} className="sm:max-w-sm rounded-lg shadow-2xl" alt='' />
+                <div className='lg:px-40'>
                     <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <PrimaryButton>Get Started</PrimaryButton>
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -5,15 +5,17 @@ import PrimaryButton from '../Shared/PrimaryButton';
 
 const MakeAppointment = () => {
     return (
-        <section style={{ background: `url(${appointment})` }} className='flex justify-center items-center'>
+        <section style={{ background: `url(${appointment})`, backgroundSize: 'cover' }} className='flex justify-center items-center mt-28'>
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-150px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1 sm:p-12 md:p-12 lg:p-6'>
+            <div className='flex-1 p-8'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
                 <h2 className='text-3xl text-white'>Make an Appoinment Today</h2>
                 <p className='text-white'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur ad autem, alias praesentium voluptatum enim deserunt assumenda minus tempore perspiciatis, eveniet reiciendis! Obcaecati, in! Iure assumenda aspernatur et distinctio totam expedita laborum ab reiciendis! Ipsa odio a tempora beatae molestias.</p>
-                <PrimaryButton>Get Started</PrimaryButton>
+                <div className='mt-5'>
+                    <PrimaryButton>Get Started</PrimaryButton>
+                </div>
             </div>
         </section>
     );
