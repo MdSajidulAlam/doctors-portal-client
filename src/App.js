9c5ee4dc9f95b91a499109c633c912a8feb0fb28
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-18 02:01:38 +0600

    Sign up implementing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appoinment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
         <Route path='/appoinment' element={<Appointment />}></Route>
       </Routes>
     </div>
