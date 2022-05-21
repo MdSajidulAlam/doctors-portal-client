c2d0bae05cff703c26aa2e52be54adfc9ec22e22
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-22 02:01:51 +0600

    Require admin implemented

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MyReview from './Pages/Dashboard/MyReview';
 import Users from './Pages/Dashboard/Users';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAdmin from './Pages/Login/RequireAdmin';
 import RequireAuth from './Pages/Login/RequireAuth';
 import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
@@ -36,7 +37,7 @@ function App() {
           <Route index element={<MyAppointment />}></Route>
           <Route path='review' element={<MyReview />}></Route>
           <Route path='history' element={<MyHistory />}></Route>
-          <Route path='users' element={<Users />}></Route>
+          <Route path='users' element={<RequireAdmin><Users /></RequireAdmin>}></Route>
         </Route>
       </Routes>
       <Toaster />
