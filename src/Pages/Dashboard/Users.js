91df56cf13569be539543d43c19a0a74f34bc5c1
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-22 01:29:45 +0600

    Admin role implemented

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -5,7 +5,12 @@ import UserRow from './UserRow';
 
 const Users = () => {
 
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/users').then(res => res.json()))
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/users', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
 
     if (isLoading) {
         return <Loading></Loading>
@@ -26,7 +31,7 @@ const Users = () => {
                     </thead>
                     <tbody>
                         {
-                            users.map(user => <UserRow key={user._id} user={user}></UserRow>)
+                            users.map(user => <UserRow key={user._id} user={user} refetch={refetch}></UserRow>)
                         }
                     </tbody>
                 </table>
