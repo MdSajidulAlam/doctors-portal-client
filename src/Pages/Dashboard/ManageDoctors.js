a2c0d719f84c51adcda6bbc03300690dd81466a2
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-23 02:29:00 +0600

    Doctor action implemented

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DeleteConfirmModal from './DeleteConfirmModal';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
+    const [deletingDoctor, setDeletingDoctor] = useState(null)
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('https://mighty-island-89854.herokuapp.com/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
@@ -14,6 +17,36 @@ const ManageDoctors = () => {
     return (
         <div>
             <h2 className='text-2xl'>Manage Doctor :{doctors.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow key={doctor._id}
+                                index={index}
+                                doctor={doctor}
+                                refetch={refetch}
+                                setDeletingDoctor={setDeletingDoctor}
+                            ></DoctorRow>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+            {deletingDoctor && <DeleteConfirmModal
+                deletingDoctor={deletingDoctor}
+                refetch={refetch}
+                setDeletingDoctor={setDeletingDoctor}
+            ></DeleteConfirmModal>}
         </div>
     );
 };
